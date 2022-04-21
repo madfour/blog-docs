c4b887ab020daefe18f896b1e801a2b136cadc5c
madfour <873912205@qq.com>
2022-04-21 16:35:58 +0800

    修改自动部署

--- deploy.sh
+++ deploy.sh
@@ -27,21 +27,6 @@ git add -A
 git commit -m "${msg}"
 git push -f $githubUrl master:gh-pages # 推送到 Github gh-pages 分支
 
-# git push -f $giteeUrl master:gh-pages # 推送到 Gitee gh-pages 分支
-
-# deploy to coding pages
-# echo 'www.YoungKbt.cn\YoungKbt.cn' > CNAME  # 自定义域名
-# echo 'google.com, pub-7828333725993554, DIRECT, f08c47fec0942fa0' > ads.txt # 谷歌广告相关文件
-
-# if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
-#   codingUrl=git@e.coding.net:xgy/xgy.git
-# else
-#   codingUrl=https://HmuzsGrGQX:${CODING_TOKEN}@e.coding.net/xgy/xgy.git
-# fi
-# git add -A
-# git commit -m "${msg}"
-# git push -f $codingUrl master # 推送到coding
-
 # 退回开始所在目录
 cd - 
 # 删除打包项目
