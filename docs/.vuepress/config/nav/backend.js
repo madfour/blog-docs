919b9781fe7d1084aaa24cec6c18b138b9855db5
madfour <873912205@qq.com>
2022-07-24 12:13:21 +0800

    up

--- docs/.vuepress/config/nav/backend.js
+++ docs/.vuepress/config/nav/backend.js
@@ -2,16 +2,14 @@
 module.exports = {
   text: '后端', link: '/backend/',
   items: [{
-    text: '语言',
+    text: 'Java',
     items: [
       { text: 'Java', link: '/java/' },
     ]
-    // }, {
-    //   text: '基础',
-    //   items: [
-    //     { text: 'JavaScript', link: '/js/' },
-    //     { text: 'HTML', link: '/html/' },
-    //     { text: 'CSS', link: '/css/' }
-    //   ]
+  }, {
+    text: '数据库',
+    items: [
+      // { text: 'JavaScript', link: '/js/' }
+    ]
   }]
 }
